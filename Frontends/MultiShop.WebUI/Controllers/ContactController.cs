ffc91abc457d1ab74ef5147e2bcb2cb10b6a2e24
dogukanakay <akay.dogukann@gmail.com>
2024-07-27 20:54:21 +0300

    Contact Page Added

--- Frontends/MultiShop.WebUI/Controllers/ContactController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -1,11 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ContactDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly string myContactApi = "https://localhost:7186/api/contacts";
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            createContactDto.SendDate = DateTime.Now;
+            createContactDto.IsRead = false;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync(myContactApi, content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
             return View();
         }
     }
---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ContactsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ContactDtos;
+using MultiShop.Catalog.Services.ContactServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllContactsList()
+        {
+            var values = await _contactService.GetAllContactsAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContactById(string id)
+        {
+            var values = await _contactService.GetByIdContactAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            await _contactService.CreateContactAsync(createContactDto);
+            return Ok("İletişim bilgisi başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteContact(string id)
+        {
+            await _contactService.DeleteContactAsync(id);
+            return Ok("İletişim bilgisi  silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateContact(UpdateContactDto updateContactDto)
+        {
+            await _contactService.UpdateContactAsync(updateContactDto);
+            return Ok("İletişim bilgisi  başarıyla güncellendi");
+        }
+    }
+}
