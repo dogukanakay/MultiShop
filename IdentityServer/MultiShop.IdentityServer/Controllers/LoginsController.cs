a3f16360fb741e28afa81c9aa29eb814b78c1f37
dogukanakay <akay.dogukann@gmail.com>
2024-07-28 03:43:37 +0300

    Jwt Generator, Login and Register Parts Added (Works)

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using MultiShop.IdentityServer.Tools;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginsController : ControllerBase
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
+		public LoginsController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UserLoginAsync(UserLoginDto userLoginDto)
+		{
+
+			var result = await _signInManager.PasswordSignInAsync(userLoginDto.UserName, userLoginDto.Password, false, false);
+			
+			if (result.Succeeded)
+			{
+				var user = await _userManager.FindByNameAsync(userLoginDto.UserName);
+				GetCheckAppUserViewModel model = new GetCheckAppUserViewModel();
+				model.UserName = userLoginDto.UserName;
+				model.Id = user.Id;
+				var token = JwtTokenGenerator.GenerateToken(model);
+				return Ok(token);
+			}
+			else
+			{
+				return BadRequest("Kullanıcı Adı veya Şifre Hatalı");
+			}
+		}
+	}
+}
---
+++ IdentityServer/MultiShop.IdentityServer/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.IdentityServer.Tools
+{
+	public class JwtTokenDefaults
+	{
+		public const string ValidAudience = "http://localhost";
+		public const string ValidIssuer = "http://localhost";
+		public const string Key = "Multishop....00101010101010101010101.netcore8.code.generator*2*42*14*21*5*215*21*5*21";
+		public const int Expire = 60;
+
+
+	}
+}
