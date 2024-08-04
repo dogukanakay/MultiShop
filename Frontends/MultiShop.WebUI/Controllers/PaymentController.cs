d51f7d7c03fb9370a67cafad838d3c286aae6422
dogukanakay <akay.dogukann@gmail.com>
2024-08-05 01:08:18 +0300

    CreditCard Payment Page Added

---
+++ Frontends/MultiShop.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Directory1 = "Ana Sayfa";
+            ViewBag.Directory2 = "Sipariş";
+            ViewBag.Directory3 = "Ödeme";
+            return View();
+        }
+    }
+}
